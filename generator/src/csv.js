dc9824ec8209bfbb8d514a0b9f55e0c2e72db95e
Serafim Suhenky <serafim@suhenky.com>
2022-07-05 12:04:25 -0600

    Introduce ENS resolution

--- generator/src/csv.js
+++ generator/src/csv.js
@@ -1,21 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+const ethers = require('ethers');
+
 
 async function exportObj() {
 
   let content = {};
   let a = {};
-  const fileStream = fs.createReadStream('./airdrop.csv');
+  const fileStream = fs.createReadStream('./src/airdrop.csv');
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity
   });
 
+  // TODO: move endpoint const to config
+  const infraEndpoint = "https://mainnet.infura.io/v3/10d0877f21884791bc511c6425ccaff2";
+  // Access network through free infura.io account
+  const provider = new ethers.providers.JsonRpcProvider(infraEndpoint);
+
   let index = 0;
   for await (const line of rl) {
     const splits = line.split(",");
-    let num = Number(splits[1])
+    let addr = String(splits[0])
+    let num = Number(splits[1]);
+
+    // Resolve address
+    let hex = await provider.resolveName(addr);
+    // Do something 
+    if(hex){
+      console.log("Resolved: " + addr + " -> " + hex);
+    }else{
+      console.log("Failed to Resolve: " + addr);
+    }
+
     a[splits[0]] = num;
     index++;
   }
