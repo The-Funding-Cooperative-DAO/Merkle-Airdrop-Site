229efff4ad4d220e8f8596bd3cdad4bcb7ed2fb1
burhanshakir <burhanshakir96@gmail.com>
2022-07-06 01:37:26 +0100

    - Added go script to run csv.js - Added web3 library and set up infura endpoint - Fetch ens from csv file and resolving to hex address

--- generator/src/csv.js
+++ generator/src/csv.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+const Web3 = require('web3')
 
 async function exportObj() {
 
+  // connect to an Infura endpoint to connect to Ethereum
+  let infuraUrl = "<INFURA_ENDPOINT>";
+  const web3 = new Web3(infuraUrl);
+
   let content = {};
   let a = {};
-  const fileStream = fs.createReadStream('./airdrop.csv');
+  const fileStream = fs.createReadStream('./src/airdrop.csv');
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity
@@ -15,8 +20,20 @@ async function exportObj() {
   let index = 0;
   for await (const line of rl) {
     const splits = line.split(",");
+    let ens = splits[0]
     let num = Number(splits[1])
     a[splits[0]] = num;
+
+    if(ens.trim().endsWith(".eth")) {
+      try {
+        const resolverAddress = await web3.eth.ens.getAddress(ens);
+        console.log(`Resolver:\t${resolverAddress}`);
+      } catch (error) {
+        console.error('Invalid address found');
+      }
+
+    }
+
     index++;
   }
 
